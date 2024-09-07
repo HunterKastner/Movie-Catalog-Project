948d51077b07f5d8715565bd9d9841651694ad86
hunter Kastner <hwkastner@gmail.com>
2024-09-07 04:07:03 -0500

    Completely Remade Javascript to work smoothly and accurately, updated CSS to work with the new javascript and simplified the HTML

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=6ac70b95
+//OMDB API: https://www.omdbapi.com/?apikey=6ac70b95&s=
 
+const movieListEl = document.querySelector(".movie-list");
+
+async function main() {
+  const movies = await fetch(
+    "https://www.omdbapi.com/?apikey=6ac70b95&s=star wars"
+  );
+  const moviesSearch = await movies.json();
+
+  const movieData = moviesSearch.Search;
+
+  movieListEl.innerHTML = movieData.map((movie) => movieHTML(movie)).join("");
+}
+
+main();
+
+function movieHTML(movie) {
+  return `<div class="movie"><figure class="movie__poster">
+                <img src="${movie.Poster}" alt="" class="movie__img" />
+              </figure>
+              <div class="movie__descriptors">
+                <p class="movie__title"><b>Title:</b> ${movie.Title}</p>
+                <p class="movie__year"><b>Year</b> ${movie.Year}</p>
+              </div></div>`;
+}
